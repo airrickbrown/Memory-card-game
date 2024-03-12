966c6c80c27d63443f8b8066980869b38c1bc4e3
Eric Ampadu Ameyaw <152802049+airrickbrown@users.noreply.github.com>
2024-03-12 05:30:15 -0700

    Create questions.js
    first commit

---
+++ qyestions.js
@@ -0,0 +1,100 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: ". What is the capital of France?",
+      answer: "Paris",
+      options: [" Berlin", "Madrid", "Paris", "Accra"],
+    },
+    {
+      numb: 2,
+      question: 'Who wrote "Romeo and Juliet"?',
+      answer: "William Shakespeare",
+      options: [
+        "William Shakespeare",
+        "Charles Dickens",
+        "Jane Austen",
+        " Mark Twain",
+      ],
+    },
+    {
+      numb: 3,
+      question: "What is the largest planet in our solar system?",
+      answer: "Jupiter",
+      options: ["Earth", "Jupiter", "Mars", "Venus"],
+    },
+    {
+      numb: 4,
+      question: "In which year did World War II end?",
+      answer: "1945",
+      options: ["1943", "1945", "1950", "1939"],
+    },
+    {
+      numb: 5,
+      question: "What is the capital of Japan?",
+      answer: "Tokyo",
+      options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+    },
+    {
+      numb: 6,
+      question: "Who painted the Mona Lisa?",
+      answer: "Leonardo da Vinci",
+      options: ["Vincent van Gogh", "Pablo Picasso", "Nana", "Leonardo da Vinci"],
+    },
+    {
+      numb: 7,
+      question: ". What is the powerhouse of the cell?",
+      answer: "Mitochondria",
+      options: [
+        "Mitochondria",
+        "Nucleus",
+        "Endoplasmic reticulum",
+        "Golgi apparatus",
+      ],
+    },
+    {
+      numb: 8,
+      question: 'Which planet is known as the "Red Planet"?',
+      answer: " Mars",
+      options: ["Venus", "Jupiter", "Saturn", " Mars"],
+    },
+    {
+      numb: 9,
+      question: 'Who is the author of "To Kill a Mockingbird"?',
+      answer: "Harper Lee",
+      options: [
+        "J.K. Rowling",
+        "Harper Lee",
+        "George Orwell",
+        "Ernest Hemingway",
+      ],
+    },
+    {
+      numb: 10,
+      question: "What is the largest ocean on Earth?",
+      answer: " Pacific Ocean",
+      options: [
+        "Atlantic Ocean",
+        "Indian Ocean",
+        "Southern Ocean",
+        " Pacific Ocean",
+      ],
+    },
+    {
+      numb: 11,
+      question: "Who developed the theory of relativity?",
+      answer: "Albert Einstein",
+      options: [
+        " Isaac Newton",
+        "Albert Einstein",
+        "Stephen Hawking",
+        "Galileo Galilei",
+      ],
+    },
+    {
+      numb: 12,
+      question: "How many continents are there on Earth?",
+      answer: "7",
+      options: ["6", "9", "7", "30"],
+    },
+  ];
